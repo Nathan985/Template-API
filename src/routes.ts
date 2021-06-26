e4b0a8c89c786ba981ce349d7eb3ca101648ffb2
Nathan985 <nathan.rodrigu3s@gmail.com>
2021-06-26 12:23:51 -0300

    Inicialização de Rota Default

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 // Rotas da API
 
-router.get('initialRoute', (req, res) => {
+router.get('/initialRoute', (req, res) => {
   res.status(200);
   return res.send({
     success: 1,
