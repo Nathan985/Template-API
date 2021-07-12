c82fc082323e2e5cb4dd6218a979e340777f3b61
Nathan985 <nathan.rodrigu3s@gmail.com>
2021-07-12 16:02:11 -0300

    Rota de Criação do Usuario

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import { createUserController } from "./useCases/CreateUserUseCase";
 
 const router = Router();
 
 // Rotas da API
+
+router.post('/createUser', (req, res) => {
+ return createUserController.handle(req, res)
+})
 
 router.get('/initialRoute', (req, res) => {
   res.status(200);
