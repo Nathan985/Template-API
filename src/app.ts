dec7799c7d5b438e77b83f948e501054632d75a5
Nathan985 <nathan.rodrigu3s@gmail.com>
2021-07-12 15:58:27 -0300

    Refatoração de Localização do Favicon

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,20 @@
 import express from 'express';
-import variables from 'dotenv';
+
+import './loadingVariables';
+
 import cors from 'cors';
 import morgan from 'morgan';
+
 import favicon from 'serve-favicon';
 import path from 'path';
 
 import { router } from './routes';
 
-variables.config({
-  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
-})
-
 const app = express();
 
 // Configurações De rotas;
 app.use(express.json()); // Uso de JSON no body das rotas
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); // Favicon da API
+app.use(favicon(path.join(__dirname, '../', 'public', 'favicon.ico'))); // Favicon da API
 app.use(cors()); // Configuração de CORS a API
 app.use(morgan("dev")); // Logs de Execução de Rotas
 
